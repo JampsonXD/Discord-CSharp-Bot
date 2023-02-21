f8d3b7cea5fe3e43b26e7737564425e9d330f1ac
JampsonXD <glennmerrill88@gmail.com>
2023-02-21 18:19:52 -0500

    Added Subscription Service / Wrapped Grammar Validation in a Try Catch

--- DiscordBot/CommandHandleService.cs
+++ DiscordBot/CommandHandleService.cs
@@ -4,9 +4,12 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using DiscordBot.Configuration;
+using DiscordBot.Core;
 using DiscordBot.Modules.DiscordEmbeds;
 using DiscordBot.Modules.Sapling;
+using DiscordBot.Modules.TimedEvents;
 using Microsoft.Extensions.Options;
+using SaplingClient.Exceptions;
 using SaplingClient.Services;
 
 namespace DiscordBot
@@ -21,7 +24,7 @@ namespace DiscordBot
         private readonly Random _random;
 
         public CommandHandleService(CommandService commands, DiscordSocketClient client, 
-            IServiceProvider services, SaplingClientService saplingClient, IOptions<DiscordBotOptions> options, Random random)
+            IServiceProvider services, SaplingClientService saplingClient, IOptions<DiscordBotOptions> options, Random random, ISubscriptionService subscriptionService)
         {
             _commands = commands;
             _client = client;
@@ -32,7 +35,22 @@ namespace DiscordBot
             
             _client.MessageReceived += ClientOnMessageReceived;
             _client.Ready += ClientOnReady;
+            subscriptionService.Subscribe<TimedTaskMessage>(OnTimeTaskMessageReceived);
         }
+
+        private void OnTimeTaskMessageReceived(TimedTaskMessage message)
+        {
+            Task.Run(() => NotifyOfTimeTaskMessageReceived(message));
+        }
+
+        private async Task NotifyOfTimeTaskMessageReceived(TimedTaskMessage message)
+        {
+            var user = _client.GetUser(message.TimedTaskInformation.DiscordUserId);
+            var action = message.Added ? "Added" : "Removed";
+            await _client.GetUser(183663101848059906).SendMessageAsync(
+                $"Time task has been {action} for discord user {user.Username} following channel id {message.TimedTaskInformation.ChannelId}");
+        }
+        
 
         private async Task ClientOnReady()
         {
@@ -81,28 +99,35 @@ namespace DiscordBot
 
         private async Task ValidateUserMessage(SocketUserMessage socketUserMessage)
         {
-            var request = _saplingClient.GrammarCorrectionResource.GrammarCheckingService();
-            request.Message = socketUserMessage.Content;
-            var response = await request.ConvertSaplingRequestIntoResponseData();
-
-            // Only alert users if our message was modified
-            if (response.IsContentModified)
+            try
             {
-                string preMessage = _options.GrammarBotSettings.DefaultGrammarResponse;
-                
+                var request = _saplingClient.GrammarCorrectionResource.GrammarCheckingService();
+                request.Message = socketUserMessage.Content;
+                var response = await request.ConvertSaplingRequestIntoResponseData();
+
+                // Only alert users if our message was modified
+                if (response.IsContentModified)
                 {
-                    var messages = _options.GrammarBotSettings.GrammarCorrectionResponses;
-                    if (messages is {Count: > 0})
-                    {
-                        preMessage = messages[_random.Next(0, messages.Count)];
-                    }
-                }
+                    string preMessage = _options.GrammarBotSettings.DefaultGrammarResponse;
                 
-                Debug.Assert(response.ModifiedMessage != null, "response.ModifiedMessage != null");
-                // Create our embedded message
-                Embed embed =
-                    GrammarCheckingEmbed.CreateGrammarCheckingEmbed(_client, preMessage, response.ModifiedMessage);
-                await socketUserMessage.Channel.SendMessageAsync(null, false, embed);
+                    {
+                        var messages = _options.GrammarBotSettings.GrammarCorrectionResponses;
+                        if (messages is {Count: > 0})
+                        {
+                            preMessage = messages[_random.Next(0, messages.Count)];
+                        }
+                    }
+                
+                    Debug.Assert(response.ModifiedMessage != null, "response.ModifiedMessage != null");
+                    // Create our embedded message
+                    Embed embed =
+                        GrammarCheckingEmbed.CreateGrammarCheckingEmbed(_client, preMessage, response.ModifiedMessage);
+                    await socketUserMessage.Channel.SendMessageAsync(null, false, embed);
+                }
+            }
+            catch (SaplingInvalidResponseException responseException)
+            {
+                Console.WriteLine($"Error in requesting Sapling Response Data. \n Error: {responseException.Message}");
             }
         }
 
