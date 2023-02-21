aab0bb46ff60d8dde9c2d86dcc53773a0dcfd878
JampsonXD <glennmerrill88@gmail.com>
2023-02-21 18:19:14 -0500

    Added Extension for Timed Task Message Updates
    Added the Timed Task Message class so that when our database adds or removes a Timed Task object, we can broadcast this change and allow the Timed Task Handler to remove any tasks found regarding that change.

---
+++ DiscordBot/Modules/TimedEvents/TimedTaskHandlerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Discord.WebSocket;
+using DiscordBot.Core;
+using DiscordBot.Models;
+using YoutubeClient.Services;
+
+namespace DiscordBot.Modules.TimedEvents;
+
+public class TimedTaskMessage : IMessage
+{
+    public required YoutubeTimedTaskInformation TimedTaskInformation { get; set; }
+    public bool Added { get; set; }
+}
+
+public static class TimedTaskHandlerExtensions
+{
+    public static void NotifyTimedTaskHandler(this TimedTaskHandler handler, TimedTaskMessage message, YoutubeClientService youtubeClient, DiscordSocketClient discordClient)
+    {
+        var info = message.TimedTaskInformation;
+        if (message.Added)
+        {
+            handler.AddTask(new NewYoutubeVideoTimedTask(info.Interval, info.ChannelId, info.DiscordUserId, youtubeClient, discordClient));
+        }
+        else
+        {
+            if (handler.FindTask(task => task is NewYoutubeVideoTimedTask youtubeTask &&
+                                         youtubeTask.DiscordUserId == info.DiscordUserId &&
+                                         youtubeTask.YoutubeChannelPlaylistId == info.ChannelId, out var foundTask) && foundTask != null)
+            {
+                handler.RemoveTask(foundTask);
+            }
+        }
+    }
+}
