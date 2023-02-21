0a462fbab704cbecb2a820fb2ce8365e59e38f63
JampsonXD <glennmerrill88@gmail.com>
2023-02-21 18:10:29 -0500

    Added Youtube Timed Task POCO Entity
    POCO class represents a table in the database holding information related to a Youtube Timed Task.

---
+++ DiscordBot/Models/YoutubeTimedTaskInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiscordBot.Models;
+
+public class YoutubeTimedTaskInformation
+{
+    [Required]
+    public required ulong DiscordUserId { get; set; }
+    [Required]
+    public required string ChannelId { get; set; }
+    [Required]
+    public required TimeSpan Interval { get; set; }
+}
