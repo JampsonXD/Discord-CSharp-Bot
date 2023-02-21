b42cbc2f4a2c28a13ddb724d92642215fbfff344
JampsonXD <glennmerrill88@gmail.com>
2023-02-21 18:08:46 -0500

    Added Local Database and Database Context
    Created a local database instance and added a database context class for handling data that needs to persist through the application.

---
+++ DiscordBot/Core/DiscordBotDatabase.cs
@@ -0,0 +1,21 @@
+﻿using DiscordBot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordBot.Core;
+
+public class DiscordBotDatabase: DbContext
+{
+    public required DbSet<YoutubeTimedTaskInformation> YoutubeTimedTaskInformation { get; set; }
+
+    public DiscordBotDatabase(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<YoutubeTimedTaskInformation>().HasKey(e => new {e.DiscordUserId, e.ChannelId});
+    }
+}
